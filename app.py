1a2dcd534632371c38a2fe4ab0d486a193314097
FADECK18 <fegaribayf@unac.edu.pe>
2026-09-09 01:13:29 -0500

    Update print statement from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import io
-import matplotlib.pyplot as plt
 
 # ============================================================
 # CONFIGURACIÓN DE LA PÁGINA
