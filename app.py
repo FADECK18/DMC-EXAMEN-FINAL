792cf3b73da1cd29bc13397ac27a432733179ece
FADECK18 <fegaribayf@unac.edu.pe>
2026-09-08 21:12:42 -0500

    Add image to sidebar in Streamlit app
    Add sidebar image to the Streamlit application

--- app.py
+++ app.py
@@ -19,6 +19,8 @@ if modulos == "Home":
         unsafe_allow_html=True
     )
 
+    st.sidebar.image("IMAGEN PYTHON_N.png", width=100)
+    
     # Breve descripción del objetivo del análisis
     st.subheader("Breve descripción del objetivo del análisis")
 
