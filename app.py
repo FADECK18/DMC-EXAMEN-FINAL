7d961622a26fe48a407aa6940a5c3dd485371940
FADECK18 <fegaribayf@unac.edu.pe>
2026-09-08 21:07:41 -0500

    Add sidebar image and format selectbox
    Added a sidebar image to the Streamlit app and adjusted the selectbox formatting.

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
+ st.sidebar.image("IMAGEN PYTHON_N.png", width=100)
+
 # Menú lateral
 st.sidebar.title("Módulo")
 
@@ -7,8 +9,7 @@ st.sidebar.image("IMAGEN DMC.png", width=100)
 
 modulos = st.sidebar.selectbox(
     "Seleccione un módulo:",
-    ["Home", "Modulo 1", "Modulo 2", "Modulo 3"]
-)
+    ["Home", "Modulo 1", "Modulo 2", "Modulo 3"])
 
 # Módulo Home
 if modulos == "Home":
@@ -17,8 +18,6 @@ if modulos == "Home":
     st.markdown(
         "<h1 align='center'>APLICACIÓN INTERACTIVA CONSTRUIDA EN PYTHON UTILIZANDO STREAMLIT</h1>",
         unsafe_allow_html=True)
-
-    st.sidebar.image("IMAGEN PYTHON_N.png", width=100)
 
     # Breve descripción del objetivo del análisis
     st.subheader("Breve descripción del objetivo del análisis")
